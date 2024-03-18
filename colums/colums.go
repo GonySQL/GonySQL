6da209ae4bb886b8baee6a246bec714b3c8921d1
mahaonan001 <1649801526@qq.com>
2024-03-18 16:46:17 +0800

    modified:   colums/colums.go

--- colums/colums.go
+++ colums/colums.go
@@ -5,12 +5,12 @@ import "reflect"
 type Colum struct {
 	Name    string
 	Type    reflect.Type
-	Content any
+	Content  colum_append
 }
 type colum_append interface {
 	Append_content(any)
 }
 
 func (c *Colum)Append_content(sty any){
-	
+	c.Content.Append_content(sty)
 }
